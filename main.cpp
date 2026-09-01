6c7f0d88cca72469d78bedc00ed7b772a1a7bf1a
Roysber Cerrada <Roysber18@gmail.com>
2026-08-31 20:45:34 -0400

    Initial commit: Estructura base del proyecto y Hola Mundo

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "=== Calculadora Cientifica y Graficadora ===" <<std::endl;
+    std::cout << "=== Proyecto iniciado existosamente ===" <<std::endl;
+
+    return 0;
+}
